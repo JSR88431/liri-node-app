43fc73caa9c55478f920e394447098902aa7026b
jsr88431 <jsr88431@gmail.com>
2018-09-17 14:52:17 -0700

    update files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # liri-node-app
+
+Check work images folder for screenshots of working liri.js app
--- liri.js
+++ liri.js
@@ -14,81 +14,66 @@ var command = process.argv[2];
 
 
 // SPOTIFY
-spotify.search({ type: 'track', query: song , limit: 3 }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
-  }
- 
-
-  if (command === "spotify-this-song"){
+if (command === "spotify-this-song") {
+  spotify.search({ type: 'track', query: song, limit: 3 }, function (err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
     console.log(data.tracks.items[0].artists[0]);
     console.log(data.tracks.items[0].name);
     console.log(data.tracks.items[0].preview_url);
     console.log(data.tracks.items[0].album.name);
   }
- 
- 
-});
+
+
+  )
+};
 
 // BANDS
+
 var artist = process.argv.slice(3).join("+");
-
-request("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp", function(error, response, body) {
-
-  if (command === "concert-this") {
+if (command === "concert-this") {
+  request("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp", function (error, response, body) {
 
     var parser = JSON.parse(body);
-    for (var i=0; i < parser.length; i++){
+    for (var i = 0; i < parser.length; i++) {
       console.log(parser[i].venue.name)
       console.log(parser[i].venue.city)
       console.log(parser[i].venue.country)
       console.log(moment(parser[i].datetime).format("MM/DD/YYYY") + "\n")
-      
-    }
 
-  }
-});
+    }
+  });
+}
+
 
 
 // MOVIES
 
 var movieName = process.argv.slice(3).join("+");
-var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+if (command === "movie-this") {
+  var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
 
-request(queryUrl, function(error, response, body) {
+  request(queryUrl, function (error, response, body) {
+    var movieParsed = JSON.parse(body);
+    console.log(movieParsed.Title);
+    console.log("Year: " + movieParsed.Year);
+    console.log("imdb Rating: " + movieParsed.imdbRating);
+    console.log("Rating: " + movieParsed.Ratings[1].Source + " " + movieParsed.Ratings[1].Value);
+    console.log("Produced in: " + movieParsed.Country);
+    console.log("Language: " + movieParsed.Language);
+    console.log("Summary: " + movieParsed.Plot);
+    console.log("Actors: " + movieParsed.Actors);
+  });
+}
 
-  
-  if (command === "movie-this") {
-  var movieParsed = JSON.parse(body);
-   console.log(movieParsed.Title);
-   console.log("Year: " + movieParsed.Year);
-   console.log("imdb Rating: " + movieParsed.imdbRating);
-   console.log("Rating: " + movieParsed.Ratings[1].Source + " " + movieParsed.Ratings[1].Value);
-   console.log("Produced in: " + movieParsed.Country);
-   console.log("Language: " + movieParsed.Language);
-   console.log("Summary: " + movieParsed.Plot);
-   console.log("Actors: " + movieParsed.Actors);
-  }
-});
-
-// WRITE TEXT
-// fs.writeFile("random.txt", "spotify-this-song I Want It That Wasy", function(err) {
-
-//   console.log("ADDED SOMETHING");
-
-// });
+if (command === "do-what-it-says") {
+  fs.readFile("./random.txt", "utf8", function (error, res) {
+    console.log(res);
+  });
 
 
-
-  if (command === "do-what-it-says"){
-    fs.readFile("random.txt", "utf8", function(error, res) {
-      console.log(res);
-    });
-
-    
-  }
- 
-  
+}
 
 
 
@@ -97,5 +82,6 @@ request(queryUrl, function(error, response, body) {
 
 
 
-   
- 
+
+
+
